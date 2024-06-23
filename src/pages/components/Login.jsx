7a01aa2fe4b1b4014bbb0a102aaa3a4139d245f7
Patryk Kulpa <patkul004@pbs.edu.pl>
2024-06-24 00:02:56 +0200

    dodano funkcje login i register

--- src/pages/components/Login.jsx
+++ src/pages/components/Login.jsx
@@ -2,7 +2,46 @@ import { useState } from "react";
 
 function Login() {
   const [active, setIsActive] = useState(false);
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
 
+  const handleChange = name => {
+    return event => {
+      setLoginData({ ...loginData, [name]: event.target.value });
+    };
+  };
+  const login = event => {
+    console.log("login");
+    event.preventDefault();
+    const data = {
+      username: document.getElementById("loginUsername").value,
+      password: document.getElementById("loginPassword").value,
+    };
+    fetch("http://127.0.0.1/blog-be/api.php?resource=users&action=login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (data) {
+          // set cookie for logged user
+          console.log("cookie");
+          document.cookie = `loggedInUser=${loginData.username}; path=/`;
+
+          alert(
+            "Witaj " + loginData.username + ", logowanie przebiegło pomyślnie"
+          );
+        }
+      })
+      .catch(error => console.error("Error:", error));
+  };
   const handleLogin = () => {
     setIsActive(!active);
   };
@@ -14,17 +53,23 @@ function Login() {
           LOGIN
         </button>
         {active ? (
-          <form id="form">
+          <form id="form" onSubmit={login}>
             <input
               type="text"
               id="loginUsername"
               placeholder="Username"
+              name="username"
+              value={loginData.username}
+              onChange={handleChange("username")}
               required
             />
             <input
               type="password"
               id="loginPassword"
               placeholder="Password"
+              name="password"
+              value={loginData.password}
+              onChange={handleChange("password")}
               required
             />
             <button className="login-btn" type="submit">
--- src/pages/components/Register.jsx
+++ src/pages/components/Register.jsx
@@ -2,6 +2,26 @@ import { useState } from "react";
 
 function Register() {
   const [activeRegister, setIsActiveRegister] = useState(false);
+  const register = event => {
+    console.log("register");
+    event.preventDefault();
+    const data = {
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+      email: document.getElementById("email").value,
+    };
+    fetch("http://127.0.0.1/blog-be/api.php?resource=users&action=register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(data => alert(data.message))
+      .catch(error => console.error("Error:", error));
+  };
   const handleRegister = () => {
     setIsActiveRegister(!activeRegister);
   };
@@ -12,7 +32,7 @@ function Register() {
           REGISTER
         </button>
         {activeRegister ? (
-          <form id="form">
+          <form id="form" onSubmit={register}>
             <input type="text" id="username" placeholder="Username" required />
             <input
               type="password"
